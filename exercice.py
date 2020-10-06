9a090d2bf3ee3e699b2182ad36d607188972ca00
leandregrimmel <leandregrimmel@gmail.com>
2020-10-06 19:38:04 -0400

    2

--- exercice.py
+++ exercice.py
@@ -3,7 +3,11 @@
 
 # TODO: Importez vos modules ici
 from math import pi
+import sys
 
+from numpy import key
+sys.path.insert(1,r"/Users/leandregrimmel/Desktop/POLY/Intro. à la programmation (INF1007)/Pycharm Projects/Exercice/c04-ch6-exercices-leandregrimmel")
+from exercise2 import frequence
 
 # TODO: Définissez vos fonction ici
 def volumeEllipsoide(a, b, c, masse_volumique):
@@ -17,3 +21,6 @@ def volumeEllipsoide(a, b, c, masse_volumique):
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     print(volumeEllipsoide(2, 4, 2, 10))
+    print((lambda sentence: sorted(frequence(sentence)), key = frequence(sentence).__getitem__)[-1])("Ceci est une phrase")
+
+    print (frequence("phrases"))
