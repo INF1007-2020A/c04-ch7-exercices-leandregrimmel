a6e73c45bf2eca009823eb9b54b209cb6b350f48
leandregrimmel <leandregrimmel@gmail.com>
2020-10-06 19:13:39 -0400

    1

--- exercice.py
+++ exercice.py
@@ -2,13 +2,18 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
+from math import pi
 
 
 # TODO: Définissez vos fonction ici
+def volumeEllipsoide(a, b, c, masse_volumique):
+    volume = 4 / 3 * pi * a * b * c
+    masse = masse_volumique * volume
+    my_tuple = (volume, masse)
+
+    return my_tuple
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    print(volumeEllipsoide(2, 4, 2, 10))
